3175cb27c61a79195f43d6a108ddb8bb3d74eb04
Mufti Alie S <iyakiwan19@gmail.com>
2021-07-16 23:53:29 +0700

    update submission 3

--- consumer/PlayListsService.js
+++ consumer/PlayListsService.js
@@ -6,15 +6,19 @@ class PlayListsService {
   }
 
   async getPlayLists(userId, playListId) {
+    console.log(userId);
+    console.log(playListId);
     const query = {
       text: `SELECT songs.id AS "id", songs.title AS "title", songs.performer AS "performer" 
       FROM songs
       INNER JOIN playlistsongs ON songs.id = playlistsongs.song_id
       INNER JOIN playlists ON playlists.id = playlistsongs.playlist_id
-      WHERE playlistsongs.playlist_id = $1 OR playlists.owner = $2`,
+      LEFT JOIN collaborations ON playlists.id = collaborations.playlist_id
+      WHERE playlistsongs.playlist_id = $1 AND (playlists.owner = $2 OR collaborations.user_id = $2)`,
       values: [playListId, userId],
     };
     const result = await this._pool.query(query);
+    console.log(result.rows);
     return result.rows;
   }
 }
